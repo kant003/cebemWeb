90812d5aff212cbe87caa1fbdb43c3f06970536c
kant003 <gonzalezm.angel@gmail.com>
2024-01-16 04:57:40 +0100

    Add Preact integration and update locales in astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   i18n: {
-    locales: ['en', 'es', 'gl'],
-    defaultLocale: 'es'
+    locales: ['es', 'gl'],
+    defaultLocale: 'es',
+    routing: {
+      prefixDefaultLocale: true
+  }
   },
   adapter: netlify()
 });
